b24ce9145ce58db2aa8082c6c38e97497ef6b6a6
leozdgao <gzdleo0816@gmail.com>
2015-03-19 17:37:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Simple static
+
+Serve folder which is defined in `config.json`. All files will be download, if directory, it will return a structrue of current path.
---
+++ index.js
@@ -0,0 +1,51 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var connect = require('connect');
+var contentDisposition = require('content-disposition');
+var config = require('./config.json');
+
+var app = connect();
+
+app.use('/file', function(req, res, next) {
+    var filePath = decodeURI(path.resolve(config.dir, req.url.slice(1)));
+    fs.stat(filePath, function(err, stat) {
+        if(err) {
+            res.statusCode = 404;
+            res.end();
+        }
+        else {
+            if(stat.isDirectory()) {
+                fs.readdir(filePath, function(err, results) {
+                    if(err) {
+                        res.statusCode = 500;
+                        res.end();
+                    }
+                    else {
+                        var structure = { dir: [], files: [] }
+                        results.forEach(function(file) {
+
+                            var stat = fs.statSync(path.resolve(filePath, file));
+                            if(stat.isDirectory()) {
+                                structure.dir.push(path.join(req.url, file));
+                            }
+                            else if(stat.isFile()) {
+                                structure.files.push(path.join(req.url, file));
+                            }
+                        });
+
+                        res.end(JSON.stringify(structure));
+                    }
+                });
+            }
+            else {
+                res.setHeader('Content-Disposition', contentDisposition(filePath));
+                fs.createReadStream(filePath).pipe(res);
+            }
+        }
+    });
+});
+
+// start server
+http.createServer(app).listen(config.port);
